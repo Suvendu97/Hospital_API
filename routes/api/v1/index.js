ad340c50096ab30f563eeb934d6b13f5d29216fd
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-08-22 18:29:00 +0530

    routes  for doctor

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -4,10 +4,4 @@ const router = express.Router();
 // route for doctors requests
 router.use('/doctors', require('./doctors'));
 
-// route for patient related requests
-router.use('/patients', require('./patients'));
-
-// route for report of patient
-router.use('/reports', require('./reports'));
-
 module.exports = router;
